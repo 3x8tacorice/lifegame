812ade8dbceedfeec2b91b708ff5f4bec661f861
tai@linux <t.nkawa.83@gmail.com>
2018-05-15 02:19:53 +0900

    Cellクラスを作成開始

--- MyFrame.java
+++ MyFrame.java
@@ -9,6 +9,10 @@ import java.time.LocalDateTime;
 
 import java.awt.Graphics;
 import java.lang.Thread;
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 
 
 public class MyFrame extends JFrame {
@@ -53,7 +57,7 @@ class MainPanel extends JPanel implements Runnable {
     JLabel dateLabel = new JLabel();
     String dateString;
     MainPanel(){
-        setBackground(Color.BLUE);
+        // setBackground(Color.BLUE);
         // add(dateLabel);
         Thread t = new Thread(this);
         t.start();
@@ -64,22 +68,55 @@ class MainPanel extends JPanel implements Runnable {
     @Override
     public void paintComponent(Graphics g){
         System.out.println("Repaint!!!");
+        super.paintComponent(g);
         // dateLabel.setText("Hello");
         this.add(dateLabel);
     }
     @Override
     public void run(){
         Integer count = 1;
+        ArrayList<Cell> cells = new ArrayList<>();
+        for (int i = 0; i < 5; i++){
+            cells.add(new Cell(Math.random() < 0.5, i, 0));
+        }
         while (true) {
             System.out.println(count);
-            dateLabel.setText(count.toString());
+            // dateLabel.setText(count.toString());
+            cells.stream()
+                .forEach(c -> {
+                    c.setIsAlive(Math.random() < 0.5);
+                    System.out.println(c.getIsAlive());
+                });
             try{
-                Thread.sleep(100);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            repaint();
+            // repaint();
             count++;
         }
+    }
+}
+class Cell {
+    boolean isAlive = true;
+    boolean nextAlive = true;
+    int pos_i = 0, pos_j = 0;
+
+    Cell(boolean isAlive, int i, int j){
+        this.setIsAlive(isAlive);
+        this.pos_i = i;
+        this.pos_j = j;
+    }
+    public void setIsAlive(boolean isAlive){
+        this.isAlive = isAlive;
+    }
+    public boolean getIsAlive(){
+        return this.isAlive;
+    }
+    public void setNextAlive(boolean isAlive){
+        this.nextAlive = isAlive;
+    }
+    public boolean getNextAlive(){
+        return this.nextAlive;
     }
 }
