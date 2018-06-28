46fed4fee917dcc4ac966a461319c9952ffc472a
tai@Mac <t.nkawa.83@gmail.com>
2018-06-28 14:21:46 +0900

    tmp commit

--- CellTest.java
+++ CellTest.java
@@ -1,9 +1,9 @@
 class CellTest {
     public static void main(String[] args) {
         Cells cellList = new Cells(25);
-        for (int x = 0; x < 5; x++){
-            for (int y = 0; y < 5; y++){
-                cellList.addCell(new Cell(Math.random()<0.5, x, y));
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 5; y++) {
+                cellList.addCell(new Cell(Math.random() < 0.5, x, y));
             }
         }
         cellList.setSurroundings();
